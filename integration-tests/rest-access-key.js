2d565722dccd2c9baba1cd55012b532c94870705
Pavel Netesa <netesa@mail.ru>
2015-02-22 11:26:26 +0200

    Add: delete tests for access key REST api.

--- integration-tests/rest-access-key.js
+++ integration-tests/rest-access-key.js
@@ -21,6 +21,7 @@ var status = {
 var url = 'http://23.253.35.56:8081/DeviceHive/rest';
 var accessKey = 'sStsRCchw3pCUeLwyVvhX/Q27CKeJgVFcNcDBvJiB6g=';
 var deviceGuid = "11111111-2222-3333-4444-555555555555";
+var deviceGuid2 = "22222222-3333-4444-5555-666666666666";
 
 //var accessKey = utils.getConfig('server:accessKey');
 var admin = {
@@ -39,15 +40,14 @@ var ownerAccessKey = '';
 
 
 var resources = [];
-var path = '/user/%d/accesskey';
 
 describe('REST Access Key', function(){
 
     //before(function () {
-    //    path = format(path, 15);
-    //    resources.push('/user/' + 40);
-    //    resources.push('/user/current/accesskey/' + 48);
-    //    resources.push('/user/current/accesskey/' + 47);
+    //    path.setOwnerId(55);
+    //    resources.push(path.get(path.USER, 55));
+    //    resources.push(path.get(path.CURRENT_ACCESS_KEY, 48));
+    //    resources.push(path.get(path.CURRENT_ACCESS_KEY, 47));
     //})
 
     before(function (done) {
@@ -64,15 +64,15 @@ describe('REST Access Key', function(){
                 }
             };
 
-            new HttpSender(url, '/user')
+            new HttpSender(url, path.USER)
                 .post(params, function (err, result) {
                     if (err) {
                         return callback(err);
                     }
 
                     var userId = result.id;
-                    resources.push('/user/' + userId);
-                    path = format(path, userId);
+                    resources.push(path.get(path.USER, userId));
+                    path.setOwnerId(userId);
                     callback();
                 });
         }
@@ -106,7 +106,7 @@ describe('REST Access Key', function(){
         it('should return all access keys', function(done){
 
             var params = { user: admin };
-            new HttpSender(url, path)
+            new HttpSender(url, path.ownerAccessKey)
                 .get(params, function (err, result, xhr) {
                     if (err) {
                         return done(err);
@@ -125,47 +125,35 @@ describe('REST Access Key', function(){
     describe('#Create()', function() {
         it('should match created and returned access key properties', function(done){
 
-            function createForAdmin(callback) {
-                var createParams = common.getParams('_integr-test-create-1');
-                common.create(path, createParams, function (err, createResult) {
-                    var getParams = { user: admin, id: createResult.id };
-                    common.get(path, getParams, function (err, getResult) {
-                        if (err) {
-                            callback(err);
-                        }
+            var testData = [
+                {
+                    createParams: common.getParams('_integr-test-create-1', admin),
+                    createPath: path.ownerAccessKey,
+                    getParams: { user: admin },
+                    getPath: path.ownerAccessKey
+                },
+                {
+                    createParams: common.getParams('_integr-test-create-2', owner),
+                    createPath: path.ownerAccessKey,
+                    getParams: { user: owner },
+                    getPath: path.CURRENT_ACCESS_KEY
+                },
+                {
+                    createParams: common.getParams('_integr-test-create-3', owner),
+                    createPath: path.ownerAccessKey,
+                    getParams: { user: owner },
+                    getPath: path.ownerAccessKey
+                },
+            ]
 
-                        assert.strictEqual(createResult.id, getResult.id);
-                        assert.strictEqual(createResult.key, getResult.key);
-                        common.expectAccessKey(getResult, createParams.data);
+            function createTest(callback) {
 
-                        callback();
-                    })
-                })
-            }
+                var td = testData.shift();
 
-            function createForOwnerCurrent(callback) {
-                var createParams = common.getParams('_integr-test-create-2');
-                common.create(path, createParams, function (err, createResult) {
-                    var getParams = { user: owner, id: createResult.id };
-                    common.get('/user/current/accesskey', getParams, function (err, getResult) {
-                        if (err) {
-                            callback(err);
-                        }
-
-                        assert.strictEqual(createResult.id, getResult.id);
-                        assert.strictEqual(createResult.key, getResult.key);
-                        common.expectAccessKey(getResult, createParams.data);
-
-                        callback();
-                    })
-                })
-            }
-
-            function createForOwner(callback) {
-                var createParams = common.getParams('_integr-test-create-3');
-                common.create(path, createParams, function (err, createResult) {
-                    var getParams = { user: owner, id: createResult.id };
-                    common.get(path, getParams, function (err, getResult) {
+                var createParams = td.createParams;
+                common.create(td.createPath, createParams, function (err, createResult) {
+                    td.getParams.id = createResult.id;
+                    common.get(td.getPath, td.getParams, function (err, getResult) {
                         if (err) {
                             callback(err);
                         }
@@ -180,9 +168,9 @@ describe('REST Access Key', function(){
             }
 
             async.series([
-                createForAdmin,
-                createForOwnerCurrent,
-                createForOwner
+                createTest,
+                createTest,
+                createTest
             ], done);
         })
     })
@@ -190,23 +178,51 @@ describe('REST Access Key', function(){
     describe('#Update()', function() {
         it('should match updated and returned access keys properties', function(done){
 
+            var testData = [
+                {
+                    updateParams: common.getParams('_integr-test-update-1', admin, new Date(2020, 4, 1),
+                        ['www.devicehive.com'], [3, 4], ['CreateDeviceNotification'], [deviceGuid], ['127.0.0.2']),
+                    updatePath: path.ownerAccessKey,
+                    getParams: { user: admin },
+                    getPath: path.ownerAccessKey
+                },
+                {
+                    updateParams: common.getParams('_integr-test-update-2', owner, new Date(2018, 3, 2),
+                        ['www.integration-tests.com'], [5, 6], ['CreateDeviceCommand'], [deviceGuid2], ['127.0.0.2']),
+                    updatePath: path.ownerAccessKey,
+                    getParams: { user: owner },
+                    getPath: path.CURRENT_ACCESS_KEY
+                },
+                {
+                    updateParams: common.getParams('_integr-test-update-3', owner, new Date(2018, 4, 15),
+                        ['www.devicehive.com'], [3, 4], ['CreateDeviceNotification', 'UpdateDeviceCommand'], [deviceGuid], ['127.0.0.2']),
+                    updatePath: path.ownerAccessKey,
+                    getParams: { user: owner },
+                    getPath: path.ownerAccessKey
+                },
+            ];
+
             function createAccessKey(callback) {
                 var createParams = common.getParams('_integr-test-update');
-                common.create(path, createParams, callback);
+                common.create(path.ownerAccessKey, createParams, function (err, createResult) {
+                    callback(err, createResult);
+                });
             }
 
-            function updateForAdmin(createResult, resource, callback) {
-                var updateParams = common.getParams('_integr-test-update-1', new Date(2020, 4, 1),
-                    ['www.devicehive.com'], [3, 4], ['CreateDeviceNotification'], [deviceGuid], ['127.0.0.2'])
+            function updateTest(createResult, callback) {
+
+                var td = testData.shift();
+                var updateParams = td.updateParams;
+
                 updateParams.id = createResult.id;
-                common.update(path, updateParams, function (err) {
+                common.update(td.updatePath, updateParams, function (err) {
 
                     if (err) {
                         callback(err);
                     }
 
-                    var getParams = { user: admin, id: createResult.id };
-                    common.get(path, getParams, function (err, getResult) {
+                    td.getParams.id = createResult.id;
+                    common.get(td.getPath, td.getParams, function (err, getResult) {
                         if (err) {
                             callback(err);
                         }
@@ -215,15 +231,67 @@ describe('REST Access Key', function(){
                         assert.strictEqual(createResult.key, getResult.key);
                         common.expectAccessKey(getResult, updateParams.data);
 
-                        callback();
+                        callback(null, createResult);
                     })
                 });
             }
 
+
             async.waterfall([
-                    createAccessKey,
-                    updateForAdmin
-                ], done);
+                createAccessKey,
+                updateTest,
+                updateTest,
+                updateTest
+            ], done);
+        })
+    })
+
+    describe('#Delete()', function() {
+        it('should return 404 when try to get delted key', function (done) {
+
+            var testData = [
+                {
+                    createParams: common.getParams('_integr-test-delete-1', admin),
+                    createPath: path.ownerAccessKey,
+                    params: { user: admin },
+                    path: path.ownerAccessKey
+                },
+                {
+                    createParams: common.getParams('_integr-test-delete-2', owner),
+                    createPath: path.ownerAccessKey,
+                    params: { user: owner },
+                    path: path.CURRENT_ACCESS_KEY
+                },
+                {
+                    createParams: common.getParams('_integr-test-delete-3', owner),
+                    createPath: path.ownerAccessKey,
+                    params: { user: owner },
+                    path: path.ownerAccessKey
+                },
+            ]
+
+            function deleteTest(callback) {
+
+                var td = testData.shift();
+
+                var createParams = td.createParams;
+                common.create(td.createPath, createParams, function (err, createResult) {
+                    td.params.id = createResult.id;
+                    common.delete(td.path, td.params, function (err) {
+                        common.get(td.path, td.params, function (err, getResult) {
+                            assert.strictEqual(!(!err), true)
+                            assert.strictEqual(err.error, 'DeviceHive server error - Access key not found.');
+                            callback();
+                        })
+                    })
+                })
+            }
+
+            async.series([
+                deleteTest,
+                deleteTest,
+                deleteTest
+            ], done);
         })
     })
 
@@ -244,13 +312,27 @@ describe('REST Access Key', function(){
     })
 });
 
+var path = {
+    USER: '/user',
+    ownerAccessKey: '/user/%d/accesskey',
+    CURRENT_ACCESS_KEY: '/user/current/accesskey',
+
+    setOwnerId: function (ownerId) {
+        this.ownerAccessKey = format(this.ownerAccessKey, ownerId);
+    },
+
+    get: function (path, id) {
+        return [path, id].join('/');
+    }
+}
+
 var common = {
 
-    getParams: function (label, expDate, domains, networkIds, actions, deviceGuids, subnets) {
+    getParams: function (label, user, expDate, domains, networkIds, actions, deviceGuids, subnets) {
         expDate || (expDate = new Date());
         expDate.setFullYear(expDate.getFullYear() + 10);
         return {
-            user: admin,
+            user: user || admin,
             data: {
                 label: label,
                 expirationDate: expDate.toISOString(),
@@ -278,27 +360,27 @@ var common = {
             }
         };
 
-        new HttpSender(url, '/user/current/accesskey')
+        new HttpSender(url, path.CURRENT_ACCESS_KEY)
             .post(params, function (err, result, xhr) {
                 if (err) {
                     return cb(err);
                 }
 
-                resources.push('/user/current/accesskey/' + result.id)
+                resources.push(path.get(path.CURRENT_ACCESS_KEY, result.id))
                 assert.strictEqual(xhr.status, status.EXPECTED_CREATED);
 
                 cb(null, result);
             });
     },
 
-    create: function (path, params, cb) {
-        new HttpSender(url, path)
+    create: function ($path, params, cb) {
+        new HttpSender(url, $path)
             .post(params, function (err, result, xhr) {
                 if (err) {
                     return cb(err);
                 }
 
-                var resource = path + '/' + result.id;
+                var resource = path.get($path, result.id);
                 resources.push(resource)
                 assert.strictEqual(xhr.status, status.EXPECTED_CREATED);
 
@@ -306,8 +388,8 @@ var common = {
             });
     },
 
-    get: function (path, params, cb) {
-        new HttpSender(url, path + '/' + params.id)
+    get: function ($path, params, cb) {
+        new HttpSender(url, path.get($path, params.id))
             .get(params, function (err, result, xhr) {
                 if (err) {
                     return cb(err);
@@ -319,14 +401,27 @@ var common = {
             });
     },
 
-    update: function (path, params, cb) {
-        new HttpSender(url, path + '/' + params.id)
+    update: function ($path, params, cb) {
+        new HttpSender(url, path.get($path, params.id))
             .put(params, function (err, result, xhr) {
                 if (err) {
                     return cb(err);
                 }
 
                 assert.strictEqual(xhr.status, status.EXPECTED_UPDATED);
+
+                cb(null);
+            });
+    },
+
+    delete: function ($path, params, cb) {
+        new HttpSender(url, path.get($path, params.id))
+            .delete(params, function (err, result, xhr) {
+                if (err) {
+                    return cb(err);
+                }
+
+                assert.strictEqual(xhr.status, status.EXPECTED_DELETED);
 
                 cb(null);
             });
