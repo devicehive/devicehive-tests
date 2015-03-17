a4479111edfa9cc3e0e18892b20215086ae7768a
Pavel Netesa <netesa@mail.ru>
2015-03-17 16:22:47 +0200

    Add: integration tests for user network REST API

---
+++ integration-tests/rest-user-network.js
@@ -0,0 +1,237 @@
+var async = require('async');
+var format = require('util').format;
+var utils = require('./common/utils');
+var path = require('./common/path');
+var status = require('./common/http').status;
+var req = require('./common/request');
+
+describe('REST API User Network', function () {
+
+    var NETWORK = '_integr-tests-user-network';
+    var networkId = null;
+    var userId = null;
+
+    before(function (done) {
+        function createUser(callback) {
+            utils.createUser2(0, void 0,
+                function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    userId = result.user.id;
+                    path.current = path.combine(path.USER, userId, path.NETWORK);
+                    callback();
+                })
+        }
+
+        function createNetwork(callback) {
+            req.create(path.NETWORK)
+                .params({user: utils.admin, data: {name: NETWORK}})
+                .send(function (err, result) {
+                    if (err) {
+                        callback(err);
+                    }
+
+                    networkId = result.id;
+                    callback();
+                });
+        }
+
+        async.series([
+            createUser,
+            createNetwork
+        ], done);
+    });
+
+    //describe('#Create', function () {
+    //    it('should create user network', function (done) {
+    //        var network = {
+    //            name: '_integr-tests-user-network-2',
+    //            description: 'lorem ipsum dolor sit amet'
+    //        };
+    //        req.create(path.current)
+    //            .params({user: utils.admin, data: network})
+    //            .send(function (err, result) {
+    //                if (err) {
+    //                    return done(err);
+    //                }
+    //
+    //                req.get(path.current)
+    //                    .params({user: utils.admin, id: result.id})
+    //                    .expect(network)
+    //                    .send(done);
+    //            });
+    //    });
+    //});
+
+    describe('#Update', function () {
+        it('should update user network', function (done) {
+            req.update(path.current)
+                .params({user: utils.admin, id: networkId})
+                .send(function (err) {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    req.get(path.current)
+                        .params({user: utils.admin, id: networkId})
+                        .expect({
+                            network: {
+                                id: networkId,
+                                name: NETWORK
+                            }
+                        })
+                        .send(done);
+                });
+        });
+    });
+
+    describe('#Delete', function () {
+        it.skip('should delete user network', function (done) {
+            req.delete(path.current)
+                .params({user: utils.admin, id: networkId})
+                .send(function (err) {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    req.get(path.current)
+                        .params({user: utils.admin, id: networkId})
+                        .expectError(status.NOT_FOUND,
+                            format('Network with id %s for user with is %s was not found', networkId, userId))// TODO: swap networkId <> userId in error message
+                        .send(done);
+                });
+        });
+    });
+
+    describe('#Not authorized', function () {
+
+        var nonNetworkUser = null;
+
+        before(function (done) {
+            utils.createUser2(1, void 0, function (err, result) {
+                if (err) {
+                    return done(err);
+                }
+
+                nonNetworkUser = result.user;
+                done();
+            });
+        });
+
+        describe('#No authorization', function () {
+            it('should fail with 401 when selecting user network by id, auth parameters omitted', function (done) {
+                req.get(path.current)
+                    .params({user: null, id: utils.NON_EXISTING_ID})
+                    .expectError(status.NOT_AUTHORIZED, 'Not authorized')
+                    .send(done);
+            });
+
+            it('should fail with 401 when updating user network with no auth parameters', function (done) {
+                req.update(path.current)
+                    .params({user: null, id: utils.NON_EXISTING_ID, data: {name: 'not-authorized'}})
+                    .expectError(status.NOT_AUTHORIZED, 'Not authorized')
+                    .send(done);
+            });
+
+            it('should fail with 401 when deleting user network with no auth parameters', function (done) {
+                req.delete(path.current)
+                    .params({user: null, id: utils.NON_EXISTING_ID})
+                    .expectError(status.NOT_AUTHORIZED, 'Not authorized')
+                    .send(done);
+            });
+        });
+
+        describe('#User authorization', function () {
+            it('should fail with 401 when creating user network with invalid user', function (done) {
+                req.get(path.current)
+                    .params({user: nonNetworkUser, id: utils.NON_EXISTING_ID})
+                    .expectError(status.NOT_AUTHORIZED, 'Not authorized')
+                    .send(done);
+            });
+
+            it('should fail with 401 when updating user network with invalid user', function (done) {
+                req.update(path.current)
+                    .params({user: nonNetworkUser, id: utils.NON_EXISTING_ID, data: {name: 'not-authorized'}})
+                    .expectError(status.NOT_AUTHORIZED, 'Not authorized')
+                    .send(done);
+            });
+
+            it('should fail with 401 when deleting user network with invalid user', function (done) {
+                req.delete(path.current)
+                    .params({user: nonNetworkUser, id: utils.NON_EXISTING_ID})
+                    .expectError(status.NOT_AUTHORIZED, 'Not authorized')
+                    .send(done);
+            });
+        });
+
+        describe('#Dummy access key authorization', function () {
+
+            var accessKey = null;
+
+            before(function (done) {
+                req.create(path.CURRENT_ACCESS_KEY)
+                    .params(utils.accessKey.getParamsObj('_integr-tests-user-network-dummy-access-key',
+                        utils.admin, void 0, void 0, void 0, ['RegisterDevice']))
+                    .send(function (err, result) {
+                        if (err) {
+                            return done(err);
+                        }
+
+                        accessKey = result.key;
+                        done();
+                    });
+            });
+
+            it('should fail with 401 when selecting user network by id using invalid access key', function (done) {
+                req.get(path.current)
+                    .params({accessKey: accessKey, id: utils.NON_EXISTING_ID})
+                    .expectError(status.NOT_AUTHORIZED, 'Not authorized')
+                    .send(done);
+            });
+
+            it('should fail with 401 when updating user network using invalid access key', function (done) {
+                req.update(path.current)
+                    .params({accessKey: accessKey, id: utils.NON_EXISTING_ID})
+                    .expectError(status.NOT_AUTHORIZED, 'Not authorized')
+                    .send(done);
+            });
+
+            it('should fail with 401 when deleting user network with no auth parameters', function (done) {
+                req.delete(path.current)
+                    .params({accessKey: accessKey, id: utils.NON_EXISTING_ID})
+                    .expectError(status.NOT_AUTHORIZED, 'Not authorized')
+                    .send(done);
+            });
+        });
+    });
+
+    describe('#Not found', function () {
+
+        it.skip('should fail with 404 when selecting user network by non-existing id', function (done) {
+            req.get(path.current)
+                .params({user: utils.admin, id: utils.NON_EXISTING_ID})
+                .expectError(status.NOT_FOUND,
+                    format('Network with id %s for user with is %s was not found', utils.NON_EXISTING_ID, userId))// TODO: swap networkId <> userId in error message
+                .send(done);
+        });
+
+        it('should fail with 404 when updating user network by non-existing id', function (done) {
+            req.update(path.current)
+                .params({user: utils.admin, id: utils.NON_EXISTING_ID})
+                .expectError(status.NOT_FOUND, format('Network with id = %s not found', utils.NON_EXISTING_ID))
+                .send(done);
+        });
+
+        it('should succeed when deleting user network by non-existing id', function (done) {
+            req.delete(path.current)
+                .params({user: utils.admin, id: utils.NON_EXISTING_ID})
+                .send(done);
+        });
+    });
+
+    after(function (done) {
+        utils.clearResources(done);
+    });
+});
