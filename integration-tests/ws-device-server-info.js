f7c54563130efa8465423e376cd51e990ce4e156
Pavel Netesa <pavel.netesa@dataart.com>
2015-03-27 22:28:20 +0200

    Add: device server info/command/notification tests

---
+++ integration-tests/ws-device-server-info.js
@@ -0,0 +1,53 @@
+var async = require('async');
+var format = require('util').format;
+var utils = require('./common/utils');
+var path = require('./common/path');
+var status = require('./common/http').status;
+var req = require('./common/request');
+var Websocket = require('./common/websocket');
+var getRequestId = utils.core.getRequestId;
+
+describe('WebSocket API Device Server Info', function () {
+    var url = null;
+
+    before(function (done) {
+        req.get(path.INFO).params({user: utils.admin}).send(function (err, result) {
+            if (err) {
+                return done(err);
+            }
+            url = result.webSocketServerUrl;
+            done();
+        });
+    });
+
+    describe('#server/info', function () {
+
+        it('should get server info, no auth', function (done) {
+            var device = new Websocket(url, 'device');
+            device.connect(function (err) {
+                if (err) {
+                    return done(err);
+                }
+
+                var requestId = getRequestId();
+                device.params({
+                        action: 'server/info',
+                        requestId: requestId
+                    })
+                    .expect({
+                        action: 'server/info',
+                        status: 'success',
+                        requestId: requestId
+                    })
+                    .assert(function (result) {
+                        utils.hasPropsWithValues(result.info, ['apiVersion', 'serverTimestamp', 'restServerUrl']);
+                    })
+                    .send(done);
+            });
+        });
+    });
+
+    after(function (done) {
+        utils.clearResources(done);
+    });
+});
