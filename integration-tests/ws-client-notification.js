12a7704b3c46ddd619bd4f5c9ff7a10c778803e1
Andrey Panevin <avpanevi@gmail.com>
2016-12-14 15:11:40 +0300

    WS client notification minor fixes

--- integration-tests/ws-client-notification.js
+++ integration-tests/ws-client-notification.js
@@ -22,6 +22,7 @@ describe('WebSocket API Client Notification', function () {
     var invalidToken = null;
 
     var clientToken = null;
+    var refreshToken = null;
     var clientInvalidToken = null;
 
     before(function (done) {
@@ -121,6 +122,11 @@ describe('WebSocket API Client Notification', function () {
             clientInvalidToken.connect(callback);
         }
 
+        function createConnRefreshTokenAuth(callback) {
+            refreshToken = new Websocket(url, 'client');
+            refreshToken.connect(callback);
+        }
+
         function authenticateWithToken(callback) {
             clientToken.params({
                     action: 'authenticate',
@@ -149,6 +155,7 @@ describe('WebSocket API Client Notification', function () {
             createInvalidToken,
             createConnTokenAuth,
             createConnInvalidTokenAuth,
+            createConnRefreshTokenAuth,
             authenticateWithToken,
             authenticateWithInvalidToken
         ], done);
@@ -156,7 +163,7 @@ describe('WebSocket API Client Notification', function () {
 
     describe('#Invalid credentials', function(done) {
         it('should return error with refresh token', function() {
-            clientToken.params({
+            refreshToken.params({
                 action: 'authenticate',
                 requestId: getRequestId(),
                 token: utils.jwt.admin_refresh
